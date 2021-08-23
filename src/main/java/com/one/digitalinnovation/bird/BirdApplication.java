5dbff54e97732be9efef42010669ba3e5377e75a
Renan Santos <renan.stos077@gmail.com>
2021-08-23 16:34:10 -0300

    initial commit

---
+++ src/main/java/com/one/digitalinnovation/bird/BirdApplication.java
@@ -0,0 +1,13 @@
+package com.one.digitalinnovation.bird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/one/digitalinnovation/bird/BirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.digitalinnovation.bird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
